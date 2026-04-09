b96facf20d5b420a6f74211633b51164b609a58c
milagrosdomingues-arg <dominguezmilagros68@gmail.com>
2026-04-09 14:00:53 -0300

    hice funcion

--- pruebaa.py
+++ pruebaa.py
@@ -1,4 +1,8 @@
-n1=10
-n2=20
-suma=n1+n2
-print(suma)
+def sumar(n1, n2):
+    suma = n1 + n2
+    return suma
+
+n1 = int(input("ingrese un numero "))
+n2 = int(input("ingrese otro numero "))
+resultado = sumar(n1, n2)
+print(resultado)
